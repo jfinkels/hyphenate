5f5d386b17308017fe6fb476f20a077c97fc8c4c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2016-12-21 00:56:05 -0500

    Removes test code from main module.

--- hyphenate.py
+++ hyphenate.py
@@ -512,12 +512,3 @@ hyphenate_word = hyphenator.hyphenate_word
 
 del patterns
 del exceptions
-
-if __name__ == '__main__':
-    import sys
-    if len(sys.argv) > 1:
-        for word in sys.argv[1:]:
-            print('-'.join(hyphenate_word(word)))
-    else:
-        import doctest
-        doctest.testmod(verbose=True)
